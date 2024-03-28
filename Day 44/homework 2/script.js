4d821f4348b695a2dc6e3f72f011476c09dde4bc
luka navrozashvili <lukanavrozas@gmail.com>
2024-03-28 23:41:19 +0400

    commit

---
+++ Day 44/homework 2/script.js
@@ -0,0 +1,7 @@
+function changeText() {
+    var text = document.getElementById("text")
+    text.textContent = "GOA IS THE BEST"
+    text.style.color = "blue"
+}
+
+myFunc()
---
+++ Day 44/homework/script.js
@@ -0,0 +1,17 @@
+//დავალება 1
+
+// function myFunc() {
+//     let num1 = prompt("Enter the first number: ")
+//     let num2 = prompt("Enter the second number: ")
+//     let num3 = prompt("Enter the third number: ")
+
+//     num1 = Number(num1)
+//     num2 = Number(num2)
+//     num3 = Number(num3)
+
+//     const sum = num1 + num2 + num3
+
+//     console.log("The sum of three numbers is", sum)
+// }
+
+// myFunc()
